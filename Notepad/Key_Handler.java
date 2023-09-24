32dab404f5e92b8d61339937f248543c0779a32f
NaveenMalviya <106728893+NaveenMalviya@users.noreply.github.com>
2023-09-24 16:23:40 +0530

    Update Key_Handler.java

--- Notepad/Key_Handler.java
+++ Notepad/Key_Handler.java
@@ -1,10 +1,19 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+// Define the GUI class
+class GUI {
+    // Sample method for demonstration
+    public void performAction(String action) {
+        System.out.println("Performed action: " + action);
+        // You can replace this with your actual file-handling logic
+    }
+}
 
-public class Key_Handler implements KeyListener {
-
-    GUI gui;
+// Create the Key_Handler class
+class Key_Handler implements KeyListener {
+    private final GUI gui;
 
     public Key_Handler(GUI gui) {
         this.gui = gui;
@@ -12,31 +21,54 @@ public class Key_Handler implements KeyListener {
 
     @Override
     public void keyTyped(KeyEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        // Not used in this example
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
         if (e.isControlDown() && e.getKeyCode() == KeyEvent.VK_S) {
-            gui.file.saveFile();
+            gui.performAction("Save");
         }
 
         if (e.isControlDown() && e.getKeyCode() == KeyEvent.VK_N) {
-            gui.file.newFile();
+            gui.performAction("New");
         }
 
         if (e.isControlDown() && e.getKeyCode() == KeyEvent.VK_O) {
-            gui.file.openFile();
+            gui.performAction("Open");
         }
 
         if (e.isShiftDown() && e.getKeyCode() == KeyEvent.VK_S) {
-            gui.file.saveAsFile();
+            gui.performAction("Save As");
         }
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        // Not used in this example
     }
+}
 
+// Create the Main class
+public class Main {
+    public static void main(String[] args) {
+        // Create an instance of the GUI class
+        GUI gui = new GUI();
+
+        // Create a JFrame
+        JFrame frame = new JFrame("Keyboard Event Handling Example");
+
+        // Create a JTextArea for demonstration
+        JTextArea textArea = new JTextArea();
+        textArea.setPreferredSize(new Dimension(300, 200));
+        textArea.addKeyListener(new Key_Handler(gui)); // Add Key_Handler as KeyListener
+
+        // Add the JTextArea to the JFrame
+        frame.add(textArea);
+
+        // Configure the JFrame
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
